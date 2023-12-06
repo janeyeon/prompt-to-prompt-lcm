684a9328daa6177f0b3777abb862e4838112dc45
janeyeon <khy5630@snu.ac.kr>
2023-12-06 16:12:38 +0900

    first commit

---
+++ ptp_utils.py
@@ -0,0 +1,426 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import numpy as np
+import torch
+from PIL import Image, ImageDraw, ImageFont
+import cv2
+from typing import Optional, Union, Tuple, List, Callable, Dict
+from IPython.display import display
+from tqdm.notebook import tqdm
+import diffusers
+import matplotlib.pyplot as plt
+import inspect
+
+def text_under_image(image: np.ndarray, text: str, text_color: Tuple[int, int, int] = (0, 0, 0)):
+    h, w, c = image.shape
+    offset = int(h * .2)
+    img = np.ones((h + offset, w, c), dtype=np.uint8) * 255
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    # font = ImageFont.truetype("/usr/share/fonts/truetype/noto/NotoMono-Regular.ttf", font_size)
+    img[:h] = image
+    textsize = cv2.getTextSize(text, font, 1, 2)[0]
+    text_x, text_y = (w - textsize[0]) // 2, h + offset - textsize[1] // 2
+    cv2.putText(img, text, (text_x, text_y ), font, 1, text_color, 2)
+    return img
+
+
+def view_images(images, num_rows=1, offset_ratio=0.02):
+    if type(images) is list:
+        num_empty = len(images) % num_rows
+    elif images.ndim == 4:
+        num_empty = images.shape[0] % num_rows
+    else:
+        images = [images]
+        num_empty = 0
+
+    empty_images = np.ones(images[0].shape, dtype=np.uint8) * 255
+    images = [image.astype(np.uint8) for image in images] + [empty_images] * num_empty
+    num_items = len(images)
+
+    h, w, c = images[0].shape
+    offset = int(h * offset_ratio)
+    num_cols = num_items // num_rows
+    image_ = np.ones((h * num_rows + offset * (num_rows - 1),
+                      w * num_cols + offset * (num_cols - 1), 3), dtype=np.uint8) * 255
+    for i in range(num_rows):
+        for j in range(num_cols):
+            image_[i * (h + offset): i * (h + offset) + h:, j * (w + offset): j * (w + offset) + w] = images[
+                i * num_cols + j]
+
+    pil_img = Image.fromarray(image_)
+    # display(pil_img)
+    return pil_img
+
+def get_scalings_for_boundary_condition_discrete(timestep):
+    sigma_data = 0.5  # Default: 0.5
+    scaled_timestep = timestep * 10.0
+
+    c_skip = sigma_data**2 / (scaled_timestep**2 + sigma_data**2)
+    c_out = scaled_timestep / (scaled_timestep**2 + sigma_data**2) ** 0.5
+    return c_skip, c_out
+
+def diffusion_step(model, controller, latents, context, i, t, guidance_scale, low_resource=False):
+    if low_resource:
+        noise_pred_uncond = model.unet(latents, t, encoder_hidden_states=context[0])["sample"]
+        noise_prediction_text = model.unet(latents, t, encoder_hidden_states=context[1])["sample"]
+    else:
+        latents_input = torch.cat([latents] * 2)
+        print(f"latent_input shape: {latents_input.shape}, context shape: {context.shape}")
+        noise_pred = model.unet(latents_input, t, encoder_hidden_states=context)["sample"]
+        noise_pred_uncond, noise_prediction_text = noise_pred.chunk(2)
+
+    noise_pred = noise_pred_uncond + guidance_scale * (noise_prediction_text - noise_pred_uncond)
+    latents, denoised = model.scheduler.step(model_output = noise_pred, timestep = t, sample=latents, timeindex=i, return_dict=False)
+
+    latents = controller.step_callback(latents)
+    
+    return latents, denoised
+
+def prepare_extra_step_kwargs(model, generator, eta):
+        # prepare extra kwargs for the scheduler step, since not all schedulers have the same signature
+        # eta (η) is only used with the DDIMScheduler, it will be ignored for other schedulers.
+        # eta corresponds to η in DDIM paper: https://arxiv.org/abs/2010.02502
+        # and should be between [0, 1]
+
+        accepts_eta = "eta" in set(inspect.signature(model.scheduler.step).parameters.keys())
+        extra_step_kwargs = {}
+        if accepts_eta:
+            extra_step_kwargs["eta"] = eta
+
+        # check if the scheduler accepts generator
+        accepts_generator = "generator" in set(inspect.signature(model.scheduler.step).parameters.keys())
+        if accepts_generator:
+            extra_step_kwargs["generator"] = generator
+        return extra_step_kwargs
+
+
+
+
+def init_latent(latent, model, height, width, generator, batch_size):
+    if latent is None:
+        latent = torch.randn(
+            (1, model.unet.config.in_channels, height // 8, width // 8),
+            generator=generator,
+        )
+    latent = latent*model.scheduler.init_noise_sigma   
+    #latent = latent.to('cuda')
+    latents = latent.expand(batch_size,  model.unet.config.in_channels, height // 8, width // 8).to(model.device)
+    return latent, latents
+
+
+@torch.no_grad()
+def text2image_ldm(
+    model,
+    prompt:  List[str],
+    controller,
+    num_inference_steps: int = 50,
+    guidance_scale: Optional[float] = 7.,
+    generator: Optional[torch.Generator] = None,
+    latent: Optional[torch.FloatTensor] = None,
+):
+    register_attention_control(model, controller)
+    height = width = 512
+    batch_size = len(prompt)
+    
+    uncond_input = model.tokenizer([""] * batch_size, padding="max_length", max_length=77, return_tensors="pt")
+    uncond_embeddings = model.text_encoder(uncond_input.input_ids.to(model.device))[0]
+    
+    text_input = model.tokenizer(prompt, padding="max_length", max_length=77, return_tensors="pt")
+    text_embeddings = model.text_encoder(text_input.input_ids.to(model.device))[0]
+    
+    latent, latents = init_latent(latent, model, height, width, generator, batch_size)
+    context = torch.cat([uncond_embeddings, text_embeddings],dim =0)
+    
+    model.scheduler.set_timesteps(num_inference_steps)
+    timesteps = model.scheduler.timesteps
+    for i, t in enumerate(tqdm(timesteps)):
+        latents, denoised = diffusion_step(model, controller, latents, context, i,t, guidance_scale)
+
+    image = model.vae.decode(denoised/model.vae.config.scaling_factor, return_dict=False)[0]
+    imageprocessor = diffusers.image_processor.VaeImageProcessor(vae_scale_factor = 2**(len(model.vae.config.block_out_channels)-1))
+    image =imageprocessor.postprocess(image)
+    image = np.array(image)
+    return image, latents
+
+
+
+@torch.no_grad()
+def text2image_ldm_stable(
+    model,
+    prompt: List[str],
+    controller,
+    num_inference_steps: int = 50,
+    guidance_scale: float = 7.5,
+    generator: Optional[torch.Generator] = None,
+    latent: Optional[torch.FloatTensor] = None,
+    low_resource: bool = False,
+):
+    register_attention_control(model, controller)
+    height = width = 512
+    batch_size = len(prompt)
+
+    text_input = model.tokenizer(
+        prompt,
+        padding="max_length",
+        max_length=model.tokenizer.model_max_length,
+        truncation=True,
+        return_tensors="pt",
+    )
+    text_embeddings = model.text_encoder(text_input.input_ids.to(model.device))[0]
+    max_length = text_input.input_ids.shape[-1]
+    uncond_input = model.tokenizer(
+        [""], padding="max_length", max_length=max_length, return_tensors="pt"
+    )
+    uncond_embeddings = model.text_encoder(uncond_input.input_ids.to(model.device))[0]
+    
+    context = [uncond_embeddings, text_embeddings]
+    context = torch.cat(context)
+    latent, latents = init_latent(latent, model, height, width, generator, batch_size)
+    model.scheduler.set_timesteps(num_inference_steps, lcm_origin_steps=50)
+    timesteps = model.scheduler.timesteps
+    for i, t in enumerate(tqdm(timesteps)):
+        latents, denoised = diffusion_step(model, controller, latents, context, i, t, guidance_scale)
+    # for t in tqdm(model.scheduler.timesteps):
+    #    latents, denoised = diffusion_step(model, controller, latents, context, t, guidance_scale, low_resource=False)
+    image = model.vae.decode(denoised/model.vae.config.scaling_factor, return_dict=False)[0]
+    imageprocessor = diffusers.image_processor.VaeImageProcessor(vae_scale_factor = 2**(len(model.vae.config.block_out_channels)-1))
+    image =imageprocessor.postprocess(image)
+    image = np.array(image)
+
+    return image, latents
+
+# def upcast_vae(self):
+#         dtype = self.vae.dtype
+#         self.vae.to(dtype=torch.float32)
+#         use_torch_2_0_or_xformers = isinstance(
+#             self.vae.decoder.mid_block.attentions[0].processor,
+#             (
+#                 AttnProcessor2_0,
+#                 XFormersAttnProcessor,
+#                 LoRAXFormersAttnProcessor,
+#                 LoRAAttnProcessor2_0,
+#             ),
+#         )
+#         # if xformers or torch_2_0 is used attention block does not need
+#         # to be in float32 which can save lots of memory
+#         if use_torch_2_0_or_xformers:
+#             self.vae.post_quant_conv.to(dtype)
+#             self.vae.decoder.conv_in.to(dtype)
+#             self.vae.decoder.mid_block.to(dtype)
+
+def get_guidance_scale_embedding(w, embedding_dim=512, dtype=torch.float32):
+        """
+        See https://github.com/google-research/vdm/blob/dc27b98a554f65cdc654b800da5aa1846545d41b/model_vdm.py#L298
+
+        Args:
+            timesteps (`torch.Tensor`):
+                generate embedding vectors at these timesteps
+            embedding_dim (`int`, *optional*, defaults to 512):
+                dimension of the embeddings to generate
+            dtype:
+                data type of the generated embeddings
+
+        Returns:
+            `torch.FloatTensor`: Embedding vectors with shape `(len(timesteps), embedding_dim)`
+        """
+        assert len(w.shape) == 1
+        w = w * 1000.0
+
+        half_dim = embedding_dim // 2
+        emb = torch.log(torch.tensor(10000.0)) / (half_dim - 1)
+        emb = torch.exp(torch.arange(half_dim, dtype=dtype) * -emb)
+        emb = w.to(dtype)[:, None] * emb[None, :]
+        emb = torch.cat([torch.sin(emb), torch.cos(emb)], dim=1)
+        if embedding_dim % 2 == 1:  # zero pad
+            emb = torch.nn.functional.pad(emb, (0, 1))
+        assert emb.shape == (w.shape[0], embedding_dim)
+        return emb
+
+def register_attention_control(model, controller):
+    def ca_forward(self, place_in_unet):
+        to_out = self.to_out
+        if type(to_out) is torch.nn.modules.container.ModuleList:
+            to_out = self.to_out[0]
+        else:
+            to_out = self.to_out
+
+        # def forward(hidden_states, encoder_hidden_states=None, attention_mask=None, temb=None,):
+        #     is_cross = encoder_hidden_states is not None
+        #     residual = hidden_states
+
+        #     if self.spatial_norm is not None:
+        #         hidden_states = self.spatial_norm(hidden_states, temb)
+
+        #     input_ndim = hidden_states.ndim
+
+        #     if input_ndim == 4:
+        #         batch_size, channel, height, width = hidden_states.shape
+        #         hidden_states = hidden_states.view(batch_size, channel, height * width).transpose(1, 2)
+
+        #     batch_size, sequence_length, _ = (
+        #         hidden_states.shape if encoder_hidden_states is None else encoder_hidden_states.shape
+        #     )
+        #     attention_mask = self.prepare_attention_mask(attention_mask, sequence_length, batch_size)
+        #     if self.group_norm is not None:
+        #         hidden_states = self.group_norm(hidden_states.transpose(1, 2)).transpose(1, 2)
+
+        #     query = self.to_q(hidden_states)
+
+        #     if encoder_hidden_states is None:
+        #         encoder_hidden_states = hidden_states
+        #     elif self.norm_cross:
+        #         encoder_hidden_states = self.norm_encoder_hidden_states(encoder_hidden_states)
+
+        #     key = self.to_k(encoder_hidden_states)
+        #     value = self.to_v(encoder_hidden_states)
+            
+        #     query = self.head_to_batch_dim(query)
+        #     key = self.head_to_batch_dim(key)
+        #     value = self.head_to_batch_dim(value)
+        #     attention_probs = self.get_attention_scores(query, key, attention_mask)
+        #     attention_probs = controller(attention_probs, is_cross, place_in_unet)
+      
+        #     hidden_states = torch.bmm(attention_probs, value)
+        #     hidden_states = self.batch_to_head_dim(hidden_states)
+
+        #     # linear proj
+        #     hidden_states = to_out(hidden_states)
+
+        #     if input_ndim == 4:
+        #         hidden_states = hidden_states.transpose(-1, -2).reshape(batch_size, channel, height, width)
+
+        #     if self.residual_connection:
+        #         hidden_states = hidden_states + residual
+
+        #     hidden_states = hidden_states / self.rescale_output_factor
+
+        #     return hidden_states
+        # return forward
+
+        def forward(x, context=None, mask=None):
+            batch_size, sequence_length, dim = x.shape
+            h = self.heads
+            q = self.to_q(x)
+            is_cross = context is not None
+            context = context if is_cross else x
+            k = self.to_k(context)
+            v = self.to_v(context)
+            q = self.reshape_heads_to_batch_dim(q)
+            k = self.reshape_heads_to_batch_dim(k)
+            v = self.reshape_heads_to_batch_dim(v)
+
+            sim = torch.einsum("b i d, b j d -> b i j", q, k) * self.scale
+
+            if mask is not None:
+                mask = mask.reshape(batch_size, -1)
+                max_neg_value = -torch.finfo(sim.dtype).max
+                mask = mask[:, None, :].repeat(h, 1, 1)
+                sim.masked_fill_(~mask, max_neg_value)
+
+            # attention, what we cannot get enough of
+            attn = sim.softmax(dim=-1)
+            attn = controller(attn, is_cross, place_in_unet)
+            out = torch.einsum("b i j, b j d -> b i d", attn, v)
+            out = self.reshape_batch_dim_to_heads(out)
+            return to_out(out)
+
+        return forward
+
+    class DummyController:
+
+        def __call__(self, *args):
+            return args[0]
+
+        def __init__(self):
+            self.num_att_layers = 0
+
+    if controller is None:
+        controller = DummyController()
+
+    def register_recr(net_, count, place_in_unet):
+        if net_.__class__.__name__ == 'Crossattention':
+            net_.forward = ca_forward(net_, place_in_unet)
+            return count + 1
+        elif hasattr(net_, 'children'):
+            for net__ in net_.children():
+                count = register_recr(net__, count, place_in_unet)
+        return count
+
+    cross_att_count = 0
+    sub_nets = model.unet.named_children()
+    for net in sub_nets:
+        if "down" in net[0]:
+            cross_att_count += register_recr(net[1], 0, "down")
+        elif "up" in net[0]:
+            cross_att_count += register_recr(net[1], 0, "up")
+        elif "mid" in net[0]:
+            cross_att_count += register_recr(net[1], 0, "mid")
+
+    controller.num_att_layers = cross_att_count
+
+    
+def get_word_inds(text: str, word_place: int, tokenizer):
+    split_text = text.split(" ")
+    if type(word_place) is str:
+        word_place = [i for i, word in enumerate(split_text) if word_place == word]
+    elif type(word_place) is int:
+        word_place = [word_place]
+    out = []
+    if len(word_place) > 0:
+        words_encode = [tokenizer.decode([item]).strip("#") for item in tokenizer.encode(text)][1:-1]
+        cur_len, ptr = 0, 0
+
+        for i in range(len(words_encode)):
+            cur_len += len(words_encode[i])
+            if ptr in word_place:
+                out.append(i + 1)
+            if cur_len >= len(split_text[ptr]):
+                ptr += 1
+                cur_len = 0
+    return np.array(out)
+
+
+def update_alpha_time_word(alpha, bounds: Union[float, Tuple[float, float]], prompt_ind: int,
+                           word_inds: Optional[torch.Tensor]=None):
+    if type(bounds) is float:
+        bounds = 0, bounds
+    start, end = int(bounds[0] * alpha.shape[0]), int(bounds[1] * alpha.shape[0])
+    if word_inds is None:
+        word_inds = torch.arange(alpha.shape[2])
+    alpha[: start, prompt_ind, word_inds] = 0
+    alpha[start: end, prompt_ind, word_inds] = 1
+    alpha[end:, prompt_ind, word_inds] = 0
+    return alpha
+
+
+def get_time_words_attention_alpha(prompts, num_steps,
+                                   cross_replace_steps: Union[float, Dict[str, Tuple[float, float]]],
+                                   tokenizer, max_num_words=77):
+    if type(cross_replace_steps) is not dict:
+        cross_replace_steps = {"default_": cross_replace_steps}
+    if "default_" not in cross_replace_steps:
+        cross_replace_steps["default_"] = (0., 1.)
+    alpha_time_words = torch.zeros(num_steps + 1, len(prompts) - 1, max_num_words)
+    for i in range(len(prompts) - 1):
+        alpha_time_words = update_alpha_time_word(alpha_time_words, cross_replace_steps["default_"],
+                                                  i)
+    for key, item in cross_replace_steps.items():
+        if key != "default_":
+             inds = [get_word_inds(prompts[i], key, tokenizer) for i in range(1, len(prompts))]
+             for i, ind in enumerate(inds):
+                 if len(ind) > 0:
+                    alpha_time_words = update_alpha_time_word(alpha_time_words, item, i, ind)
+    alpha_time_words = alpha_time_words.reshape(num_steps + 1, len(prompts) - 1, 1, 1, max_num_words)
+    return alpha_time_words
